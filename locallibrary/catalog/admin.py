cb1989c76573d5692adb9f1850a81da61598bffd
Alex Grand <namelost@protonmail.com>
2020-09-30 18:52:50 +0300

    added features to admin.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -10,19 +10,42 @@ admin.site.register(Language)
 
 # admin.site.register(Author)
 # Define the admin class
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0 # num of book instamces fields to show up
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BooksInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0 # num of book instamces fields to show up
 
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
+    #adds name of fields in editor mode, groups fields
+    fieldsets = (
+        ('General', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Avalability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
