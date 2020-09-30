b1b6844988f4b2802a317c070866873c8a60255a
Alex Grand <namelost@protonmail.com>
2020-09-30 18:34:56 +0300

    added filters to ModelAdmin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -24,4 +24,5 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
